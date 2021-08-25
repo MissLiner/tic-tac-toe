770a8f11eacc92dd585cd529b82be774d2f1620a
MissLiner <miss.liner@yahoo.com>
2021-08-25 16:26:16 -0400

    added board, controller and players

--- script.js
+++ script.js
@@ -1,3 +1,47 @@
-let Gameboard = {
-    box1: 
-}
+const Gameboard = (function() {
+    const board = ['O', 'X', 'O', 'X', 'X', 'X', 'O', 'O', 'X'];
+    return board;
+})()
+
+const gameBoxes = document.querySelectorAll('.game-box');
+
+(gameController = () => {
+    let turn = 'player1';
+    gameBoxes.forEach(box => {
+        box.addEventListener('click', () => {
+            if (turn === 'player1') {
+                box.textContent = 'X';
+                Gameboard[this.id] = 'X';
+                turn = 'player2';
+            }
+            else {
+                box.textContent = 'O';
+                Gameboard[this.id] = 'O';
+                turn = 'player1';
+            }
+        })
+    })
+    for (i = 0; i < 9; i++) {
+        gameBoxes[i].textContent = Gameboard[i];
+    }
+})()
+
+const playerFactory = (name, marker) => {
+    this.name = name;
+    this.marker = marker;
+    return {name, marker};
+};
+
+const player1 = playerFactory('Caroline', 'x');
+const player2 = playerFactory('Annabelle', 'o');
+
+
+
+//create a gameboard that is 3x3
+//name each box
+//create two players
+//allow each player to be assigned xs or os
+//log which box a player puts their marker into
+//notice when three of the same marker are in a row
+//declare a winner
+//cleat the board
