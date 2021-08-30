0bba0953059cfe5469009a085c6bd711640217b5
MissLiner <miss.liner@yahoo.com>
2021-08-30 15:50:39 -0400

    game is working now, forgot to commit a lot

--- script.js
+++ script.js
@@ -1,49 +1,75 @@
-const Gameboard = (function() {
-    const board = ['', '', '', '', '', '', '', '', ''];
-    return board;
-})()
-
 const gameBoxes = document.querySelectorAll('.game-box');
-
-(gameController = () => {
-    let turn = 'player1';
-    gameBoxes.forEach(box => {
-        box.addEventListener('click', () => {
-            if (box.textContent === '') {
-                if (turn === 'player1' && box.textContent === '') {
-                    box.textContent = 'X';
-                    Gameboard[box.id] = 'X';
-                    turn = 'player2';
-                }
-                else {
-                    box.textContent = 'O';
-                    Gameboard[box.id] = 'O';
-                    turn = 'player1';
-                }
-            }
-        })
-    })
-    // for (i = 0; i < 9; i++) {
-    //     gameBoxes[i].textContent = Gameboard[i];
-    // }
-})()
+const gameMessages = document.getElementById('game-messages');
 
 const playerFactory = (name, marker) => {
     this.name = name;
     this.marker = marker;
-    return {name, marker};
+    this.wins = 0;
+    return {name, marker, wins};
 };
 
 const player1 = playerFactory('Caroline', 'x');
 const player2 = playerFactory('Annabelle', 'o');
 
+const Gameboard = (() => {
+    const board = ['', '', '', '', '', '', '', '', ''];
 
+    checkWin = (a, b, c) => {
+        if () {
 
-//create a gameboard that is 3x3
-//name each box
-//create two players
-//allow each player to be assigned xs or os
-//log which box a player puts their marker into
-//notice when three of the same marker are in a row
-//declare a winner
-//cleat the board
+        }
+        else if (a && a === b && b === c) {
+            outcome = 'gameOver';
+            if (a === '1') {
+                gameMessages.classList.remove('hidden');
+                gameMessages.textContent = `${player1.name} wins!`;
+            }
+            else if (a === '2') {
+                gameMessages.classList.remove('hidden');
+                gameMessages.textContent = `${player2.name} wins!`;
+            }
+            }
+        }
+    }
+    endGame = () => {
+    checkWin(board[0], board[1], board[2]);
+    checkWin(board[3], board[4], board[5]);
+    checkWin(board[6], board[7], board[8]);
+    checkWin(board[0], board[3], board[6]);
+    checkWin(board[1], board[4], board[7]);
+    checkWin(board[2], board[5], board[8]);
+    checkWin(board[0], board[4], board[8]);
+    checkWin(Gameboard.board[2], Gameboard.board[4], Gameboard.board[6]);
+    }
+    return {
+        board,
+        outcome,
+        endGame
+    }
+})()
+
+const gameController = (() => {
+    let turn = 'player1';
+    let outcome = 'none';
+    let turnCounter = 0;
+    gameBoxes.forEach(box => {
+        box.addEventListener('click', () => {
+            // if (Gameboard.outcome === 'gameOver') {
+            //     clearBoard = () => gameBoxes.forEach(box => box.textContent = '');
+            // }
+             if (box.textContent === '') {
+                if (turn === 'player1' && box.textContent === '') {
+                    box.textContent = 'X';
+                    Gameboard.board[box.id] = '1';
+                    turn = 'player2';
+                }
+                else {
+                    box.textContent = 'O';
+                    Gameboard.board[box.id] = '2';
+                    turn = 'player1';
+                };
+            };
+            Gameboard.endGame();
+        });
+    });
+})()
